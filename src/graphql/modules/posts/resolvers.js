dcf65d3614b5d151aaccf7bb7623754af7427585
AndersonAlpin <andersonalpin@hotmail.com>
2021-10-16 14:35:13 -0300

    add posts resolvers and users resolvers

--- src/graphql/modules/posts/resolvers.js
+++ src/graphql/modules/posts/resolvers.js
@@ -1,4 +1,13 @@
+import Post from '../../../models/Post';
+
 export default {
-  Query: {},
-  Mutation: {}
+  Query: {
+    Posts: () => Post.find(),
+    Post: (_, { id }) => Post.findById(id),
+  },
+  Mutation: {
+    createPost: (_, { postInput }) => Post.create(postInput),
+    updatePost: (_, { id, postInput }) => Post.findOneAndUpdate(id, postInput, { new: true }),
+    deletePost: async (_, { id }) => !!(await Post.findOneAndDelete(id)),
+  }
 }
--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,4 +1,13 @@
+import User from '../../../models/User';
+
 export default {
-  Query: {},
-  Mutation: {}
+  Query: {
+    users: () => User.find(),
+    user: (_, { id }) => User.findById(id),
+  },
+  Mutation: {
+    createUser: (_, { userInput }) => User.create(userInput),
+    updateUser: (_, { id, userInput }) => User.findOneAndUpdate(id, userInput, { new: true }),
+    deleteUser: async (_, { id }) => !!(await User.findOneAndDelete(id)),
+  }
 }
